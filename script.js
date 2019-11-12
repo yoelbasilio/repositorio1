5ea14beaee0d1a13cbca91d1060298d15cf9612b
yoelbasilio <57687978+yoelbasilio@users.noreply.github.com>
2019-11-12 18:52:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function borrar(){
+    var piso= document.getElementById("piso");
+    var piso= document.getElementById("piso");
+    var piso= document.getElementById("piso");
+}
+let piso= document.getElementById("piso").nodeValue;
